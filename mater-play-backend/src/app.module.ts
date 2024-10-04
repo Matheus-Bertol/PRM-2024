91eccf72b4c794fadf980c0057438af6a4908a61
Matheus-Bertol <matheusbertol76@gmail.com>
2024-10-03 21:57:33 -0300

    AULA 03/10
    2.5.6 Conectando o Backend No Bando de Dados e 2.5.7 Implementando o Caso de Uso Listar Filmes

--- mater-play-backend/src/app.module.ts
+++ mater-play-backend/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
